e1e14b0ac6c5ce40c34b7ef4df254ca81908159c
jmacfar <jmacfar@gmail.com>
2019-08-24 12:58:50 -0700

    Remove (i think) all switf references

--- wrapper.sh
+++ wrapper.sh
@@ -14,12 +14,7 @@ schedulableInstanceRam=${schedulableInstanceRam:-4096}
 schedulableInstanceVcpu=${schedulableInstanceVcpu:-2}
 schedulableInstanceDisk=${schedulableInstanceDisk:-20}
 useNovaVolumes=${useNovaVolumes:-True}
-swiftHosts=${swiftHosts:-host1.example.com host2.example.com}
-#keystoneTenantsMap="firstname,1234567890 secondname,0987654321"
 resellerPrefix=${resellerPrefix:-AUTH_}
-ringPath=${ringPath:-/etc/swift}
-#hashPathPrefix=
-#hashPathSuffix=
 
 if [ ! -e "${configFile}" ]; then
     mkdir -p ${prometheusDir}
@@ -42,22 +37,11 @@ if [ ! -e "${configFile}" ]; then
     done
     sed -i '/.*VAR_ENABLED_COLLECTORS.*/d'					${configFile} 
 
-    for i in ${swiftHosts}; do
-        sed -i "s/.*VAR_SWIFT_HOSTS/  - ${i}\n  - VAR_SWIFT_HOSTS/g" 		${configFile}
-    done
-    sed -i '/.*VAR_SWIFT_HOSTS.*/d'						${configFile} 
-
     for i in ${keystoneTenantsMap}; do
         tenantName=$(echo ${i} | cut -d',' -f1)
         tenantId=$(  echo ${i} | cut -d',' -f2)
         sed -i "s/.*VAR_KEYSTONE_TENANTS_MAP/  - ${tenantName} ${tenantId}\n  - VAR_KEYSTONE_TENANTS_MAP/g" ${configFile}
     done
-    sed -i '/.*VAR_KEYSTONE_TENANTS_MAP.*/d' 					${configFile}
-
-    sed -i "s|VAR_RESELLER_PREFIX|${resellerPrefix}|g"	 			${configFile}
-    sed -i "s|VAR_RING_PATH|${ringPath}|g"	 				${configFile}
-    sed -i "s|VAR_HASH_PATH_PREFIX|${hashPathPrefix}|g"	 			${configFile}
-    sed -i "s|VAR_HASH_PATH_SUFFIX|${hashPathSuffix}|g"	 			${configFile}
     
     sed -i 's/VAR_.*//g'		 					${configFile}
 
